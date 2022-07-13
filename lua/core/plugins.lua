ac20b763c09fe301f58585805a09921c078e390c
GeeKaven <i@tawawa.moe>
2022-07-13 21:43:01 +0800

    feat: add treesitter

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,4 +1,4 @@
-local yu_packer = require("core.packer")
+local yu_packer = require("core.pack")
 local packer = yu_packer.packer
 local use = packer.use
 
@@ -9,7 +9,7 @@ vim.cmd([[
   augroup end
 ]])
 
-return packer.startup(function ()
+return packer.startup(function()
   use({
     'wbthomason/packer.nvim',
     'nvim-lua/plenary.nvim',
@@ -28,21 +28,21 @@ return packer.startup(function ()
   use({
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
-    config = function ()
+    config = function()
       require("plugins.nvim-cmp")
     end,
     requires = {
       { "hrsh7th/cmp-path", after = "nvim-cmp" },
       { "hrsh7th/cmp-buffer", after = "nvim-cmp" },
       { "hrsh7th/cmp-nvim-lua", after = "nvim-cmp" },
-      { "hrsh7th/cmp-nvim-lsp", after = "nvim-lspconfig" },
+      -- { "hrsh7th/cmp-nvim-lsp", after = "nvim-lspconfig" },
       { "saadparwaiz1/cmp_luasnip", after = "LuaSnip" }
     }
   })
 
   use({
     "L3MON4D3/LuaSnip",
-    config = function ()
+    config = function()
       require("plugins.luasnip")
     end,
     after = "nvim-cmp",
@@ -51,7 +51,7 @@ return packer.startup(function ()
 
   use({
     "nvim-telescope/telescope.nvim",
-    config = function ()
+    config = function()
       require("plugins.telescope")
     end,
     requires = {
@@ -60,6 +60,28 @@ return packer.startup(function ()
       { "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
     },
     event = "BufWinEnter"
+  })
+
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    run = ':TSUpdate',
+    event = 'BufReadPost',
+    config = function()
+      require("plugins.treesitter")
+    end,
+    requires = {
+      { "windwp/nvim-ts-autotag", after = "nvim-treesitter"},
+      { "JoosepAlviste/nvim-ts-context-commentstring", after = "nvim-treesitter"},
+      { "nvim-treesitter/nvim-treesitter-textobjects", after = "nvim-treesitter"},
+      { "p00f/nvim-ts-rainbow", after = "nvim-treesitter"},
+      {
+        "andymass/vim-matchup",
+        after = "nvim-treesitter",
+        config = function()
+          vim.cmd([[let g:matchup_matchparen_offscreen = {'method': 'popup'}]])
+        end
+      },
+    }
   })
 
   if yu_packer.first then
